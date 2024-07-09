3843eb7b836fb132a9ab4d7edf71860db5646668
Fajar Sidik Prasetio <fajarsidik1999@gmail.com>
2024-07-09 20:52:47 +0700

    fix: riwayat

--- app/Http/Controllers/user/RiwayatController.php
+++ app/Http/Controllers/user/RiwayatController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Log;
 use URL;
 
 class RiwayatController extends Controller
@@ -33,23 +34,54 @@ class RiwayatController extends Controller
             ->whereMonth('absensis.created_at', '=', $month)
             ->whereYear('absensis.created_at', '=', $year)
             ->select(['absensis.jenis', 'absensis.created_at'])
+            ->orderBy('created_at')
             ->get();
 
-        $riwayat = $riwayat->map(function ($value) {
+        $result = [];
+
+        foreach ($riwayat as $key => $value) {
+            $formattedTime = Carbon::parse($value->created_at)->translatedFormat('H.i');
             $formattedDate = Carbon::parse($value->created_at)->translatedFormat('d F Y');
-            $formattedTime = Carbon::parse($value->created_at)->translatedFormat('H.i.s');
+            $dayOut = Carbon::parse($value->created_at)->translatedFormat('d');
+            Log::debug("value pertama {$value->jenis}");
+            if ($value->jenis == 'PULANG') {
+                $last = count($result) - 1;
+
+                if ($last > 0) {
+
+                    if ($result[$last]->jenis == 'MASUK') {
+                        $dayIn = Carbon::parse($value->created_at)->translatedFormat('d');
+                        if ($dayIn == $dayOut) {
+                            $result[$last]->pulang = $formattedTime;
+                        } else {
+                            $value->pulang = $formattedTime;
+                        }
+                    } else {
+                        $value->pulang = $formattedTime;
+                    }
+                } else {
+                    $value->pulang = $formattedTime;
+                    $value->date = $formattedDate;
+                    $data = json_decode(json_encode($value), true);
+                    array_push($result, (object) $data);
+                }
+
+                Log::debug('di masuk' . json_encode($result));
+
+                continue;
+            }
             if ($value->jenis == 'MASUK') {
                 $value->masuk = $formattedTime;
-            } else {
-                $value->pulang = $formattedTime;
             }
+            Log::debug(json_encode($result));
             $value->date = $formattedDate;
-            return $value;
-        });
-
+            $data = json_decode(json_encode($value), true);
+            array_push($result, (object) $data);
+            // array_splice
+        }
         return view('user_app.riwayat.index', with([
             'route' => $url,
-            'history' => $riwayat,
+            'history' => array_reverse($result),
             'bulan' => $bulan
         ]));
     }
